896d78deb13c112db2cdeff262d04077b200fb6a
Addison <shangxindi@gmail.com>
2023-01-29 21:31:03 +0800

    chore: remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
